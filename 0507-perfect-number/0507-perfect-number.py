70490d3439f52ee0c9e14aa2ddf6a8516e42ac63
Bhanutejareddyk <bhanu93534@gmail.com>
2026-07-10 09:42:21 +0530

    Time: 3 ms (89.39%), Space: 19.3 MB (49.94%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <=1:
+            return False
+        divisor_sum = 1;    
+        for i in range(2,int(math.isqrt(num))+1):
+            if num % i == 0:
+                divisor_sum += i
+                if i != num//i:
+                    divisor_sum += num//i
+        return divisor_sum == num             
+        
